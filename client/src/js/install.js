f0b8bf90816265684291842aa3ecbc5cd16da77d
Nick Martin <martin.nicholas.r@gmail.com>
2022-05-16 00:49:14 -0600

    everything appears to be working... having trouble retesting app install... deploying to heroku now

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,32 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', () => {
+    console.log('clicked install');
+    const eventPrompt = window.deferredPrompt;
+
+    if (!eventPrompt) {
+        return;
+    }
+
+    eventPrompt.prompt();
+    butInstall.setAttribute('disabled', true);
+
+    butInstall.textContent = 'Installed!';
+
+    butInstall.classList.toggle('hidden', true);
+  });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);    
+});
